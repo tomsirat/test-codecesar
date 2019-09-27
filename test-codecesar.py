9ac44a547e0c2a9148b2b2aca6b4a7dc27164768
tomsirat <sirat.tom@outlook.com>
2019-09-27 10:49:13 +0200

    END with modulo

--- test-codecesar.py
+++ test-codecesar.py
@@ -1,12 +1,12 @@
 alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-word = "abc"
-offset = 3
+word = "nez"
+offset = 1
 new_word = ""
 
 for i in range (len(word)):
     letter = word[i]
     position_in_alphabet = alphabet.index(letter)
-    new_position_alphabet = position_in_alphabet + offset
+    new_position_alphabet = (position_in_alphabet + offset) % len(alphabet)
     new_letter = alphabet[new_position_alphabet]
     new_word += new_letter
 print (new_word)
