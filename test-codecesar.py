a886287a699cedc2f70e5d8efb66eb8392f0204c
tomsirat <sirat.tom@outlook.com>
2019-09-27 10:10:37 +0200

    END

--- test-codecesar.py
+++ test-codecesar.py
@@ -1,6 +1,13 @@
-lst_lettre=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+word = "abc"
+offset = 3
+new_word = ""
 
-mot = "abc"
-
-for i in range (len(list_lettre))
+for i in range (len(word)):
+    letter = word[i]
+    position_in_alphabet = alphabet.index(letter)
+    new_position_alphabet = position_in_alphabet + offset
+    new_letter = alphabet[new_position_alphabet]
+    new_word += new_letter
+print (new_word)
 
